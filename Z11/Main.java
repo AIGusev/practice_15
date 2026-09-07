0982cbe2b1113b383fcc370d8bdee56d92f409d7
AIGusev <artemgusevjob94@gmail.com>
2026-09-07 20:30:45 +0300

    Update Main.java

--- Z11/Main.java
+++ Z11/Main.java
@@ -4,8 +4,8 @@ public class Main {
     static void main(String[] args) {
 
         DoorFacade doorFacade = new DoorFacade();
-        doorFacade.doorOpen.open();
-        doorFacade.doorClose.close();
-        doorFacade.doorLock.lock();
+        doorFacade.openDoor();
+        doorFacade.closeDoor();
+        doorFacade.lockDoor();
     }
 }
